91295ae90945f408142976707290821a56748acd
Adam Leung <centribo@hotmail.com>
2016-03-28 16:40:46 -0400

    Started work on campfire scene

---
+++ Assets/Scripts/CampfireLight.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CampfireLight : MonoBehaviour {
+
+	public float minIntensity;
+	public float maxIntensity;
+	public float flickerRate;
+
+	Light light;
+	float x = 0;
+	float timer = 0;
+	Vector3 originalPos;
+
+	void Awake(){
+		light = GetComponent<Light>();
+		originalPos = transform.position;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		light.intensity = minIntensity + Mathf.PerlinNoise(x, 0) * maxIntensity;
+		x += flickerRate;
+		timer += Time.deltaTime;
+		if(timer >= flickerRate){
+			transform.position = originalPos + Random.insideUnitSphere * 0.05f;
+			timer = 0;	
+		}
+		
+	}
+}
